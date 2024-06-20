afec5d7d15c806bf83f5b8533884bbfd586cfb53
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-20 14:48:34 +0900

    revise

--- app.js
+++ app.js
@@ -33,30 +33,30 @@ ball.addEventListener("click", () => {
   ball.classList.toggle("active");
 });
 
-const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    document.querySelector(".matrix-background").appendChild(canvas);
+// const canvas = document.createElement("canvas");
+//     const ctx = canvas.getContext("2d");
+//     document.querySelector(".matrix-background").appendChild(canvas);
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
 
-    const letters = Array(256).join("1").split("");
+//     const letters = Array(256).join("1").split("");
 
-    const draw = () => {
-        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#0F0";
-        letters.map((y_pos, index) => {
-            const text = String.fromCharCode(48 + Math.random() * 32);
-            const x_pos = index * 10;
-            ctx.fillText(text, x_pos, y_pos);
-            letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
-        });
-    };
+//     const draw = () => {
+//         ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+//         ctx.fillRect(0, 0, canvas.width, canvas.height);
+//         ctx.fillStyle = "#0F0";
+//         letters.map((y_pos, index) => {
+//             const text = String.fromCharCode(48 + Math.random() * 32);
+//             const x_pos = index * 10;
+//             ctx.fillText(text, x_pos, y_pos);
+//             letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+//         });
+//     };
     
-    setInterval(draw, 33);
+//     setInterval(draw, 33);
 
-    window.addEventListener('resize', () => {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    });
+//     window.addEventListener('resize', () => {
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight;
+//     });
